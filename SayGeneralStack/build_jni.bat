63a0b7e885f3bd60fd551966d403b624d7f31d26
SayCV <sssxyc@163.com>
2013-12-24 18:02:01 +0800

    ...

--- SayGeneralStack/build_jni.bat
+++ SayGeneralStack/build_jni.bat
@@ -5,7 +5,20 @@ set HOME=%cd%
 set NDK=D:/Android/android-ndk-r9b
 
 :::bash.exe --login -c "cd $(cygpath -u 'd:\Android\projects\hello-jni') && pwd && $NDK/ndk-build"
-bash.exe --login -c "cd jni && pwd && $NDK/ndk-build"
+rem bash.exe --login -c "cd jni && pwd && $NDK/ndk-build"
 
-rem cp libs/armeabi/saysu res/raw/saysu
+cd ../RevTethering
+if not exist libs (
+	mkdir libs
+	cd libs
+	if not exist armeabi (
+ 		mkdir armeabi
+	)
+	cd ..
+)
+cd ..
+
+cp SayGeneralStack/libs/armeabi/libutils_armv5te.so RevTethering/libs/armeabi/
+cp ../imsdroid/native-debug/libs/armeabi/libtinyWRAP.so RevTethering/libs/armeabi/libtinyWRAP_armv5te.so
+
 pause
