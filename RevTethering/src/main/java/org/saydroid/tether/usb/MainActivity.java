6f9d1dcebae68f7505bc3d3e5cad5a28c58614c4
SayCV <sssxyc@163.com>
2013-12-26 01:01:39 +0800

    ...

--- RevTethering/src/main/java/org/saydroid/tether/usb/MainActivity.java
+++ RevTethering/src/main/java/org/saydroid/tether/usb/MainActivity.java
@@ -1,5 +1,6 @@
 package org.saydroid.tether.usb;
 
+import android.app.Activity;
 import android.app.ActivityGroup;
 import android.content.Intent;
 import android.media.AudioManager;
@@ -112,12 +113,10 @@ public class MainActivity extends ActivityGroup {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        Log.d(TAG, "Krupa - onOptionsItemSelected");
+        IBaseScreen baseScreen = mScreenService.getCurrentScreen();
+        if(baseScreen instanceof Activity){
+            return ((Activity)baseScreen).onOptionsItemSelected(item);
         }
         return super.onOptionsItemSelected(item);
     }
