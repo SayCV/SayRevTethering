44b28901cc99115b9800d01a84d8d76af3936e0d
SayCV <sssxyc@163.com>
2014-01-12 01:53:15 +0800

    adjust layout: screen_manual_item_link.xml

--- RevTethering/src/main/java/org/saydroid/tether/usb/Screens/ScreenManual.java
+++ RevTethering/src/main/java/org/saydroid/tether/usb/Screens/ScreenManual.java
@@ -18,23 +18,41 @@
 
 package org.saydroid.tether.usb.Screens;
 
+import org.saydroid.sgs.services.ISgsConfigurationService;
 import org.saydroid.tether.usb.CustomExtends.NetworkLinkStatus;
 import org.saydroid.tether.usb.SRTDroid;
 import org.saydroid.tether.usb.R;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ScreenManual extends BaseScreen {
 	private static final String TAG = ScreenAbout.class.getCanonicalName();
-	
+
+    private ListView mListView;
+    private final static NetworkLinkStatusItem[] sListViewStatusItems = new NetworkLinkStatusItem[] {
+            new NetworkLinkStatusItem(R.drawable.user_online_24, new NetworkLinkStatus(0)),
+            new NetworkLinkStatusItem(R.drawable.user_busy_24, new NetworkLinkStatus(0)),
+    };
+
+    private final ISgsConfigurationService mConfigurationService;
+
 	public ScreenManual() {
 		super(SCREEN_TYPE.MANUAL_T, TAG);
+
+        mConfigurationService = getEngine().getConfigurationService();
 	}
 
 	@Override
@@ -42,8 +60,8 @@ public class ScreenManual extends BaseScreen {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_manual);
 
-
-
+        mListView = (ListView) findViewById(R.id.screen_manual_listView);
+        mListView.setAdapter(new ScreenNetworkLinkAdapter(this));
 	}
 
     @Override
@@ -64,6 +82,50 @@ public class ScreenManual extends BaseScreen {
         private NetworkLinkStatusItem(int drawableId, NetworkLinkStatus status) {
             mDrawableId = drawableId;
             mStatus = status;
+        }
+    }
+
+    static class ScreenNetworkLinkAdapter extends BaseAdapter {
+
+        private final LayoutInflater mInflater;
+
+        private ScreenNetworkLinkAdapter(Context context){
+            mInflater = LayoutInflater.from(context);
+        }
+
+        public int getCount() {
+            return sListViewStatusItems.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return sListViewStatusItems[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            final NetworkLinkStatusItem item = (NetworkLinkStatusItem)getItem(position);
+
+            if (view == null) {
+                view = mInflater.inflate(R.layout.screen_manual_item_link, null);
+            }
+
+            if (item == null) {
+                return view;
+            }
+
+            /*((ListView) view .findViewById(R.id.screen_manual_listView))
+                    .setImageResource(item.mDrawableId);
+            ((TextView) view.findViewById(R.id.screen_presence_status_item_textView))
+                    .setText(item.mText);*/
+
+            return view;
         }
     }
 }
